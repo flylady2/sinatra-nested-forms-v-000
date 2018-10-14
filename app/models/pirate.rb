0b450df5348c06db568b1afc640e73e1f533187e
Leslie Stevens <flylady@mail.utexas.edu>
2018-10-14 16:22:01 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Pirate
   attr_accessor :name, :weight, :height
 
   PIRATES = []
 
   def initialize(params)
+    #binding.pry
     @name = params[:name]
     @weight = params[:weight]
     @height =params[:height]
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,12 +1,14 @@
+require 'pry'
 class Ship
   attr_accessor :name, :type, :booty
 
   SHIPS = []
 
-  def initialize(args)
-    @name = args[:name]
-    @type = args[:type]
-    @booty = args[:booty]
+  def initialize(details)
+    #binding.pry
+    @name = details[:name]
+    @type = details[:type]
+    @booty = details[:booty]
     SHIPS << self
   end
 
